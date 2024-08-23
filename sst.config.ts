a98792476ce60c31a1e62eafad7b7ff4ca287a80
Paul Bacchus <paulbacchus@M35-117.local>
2024-08-23 16:03:56 +0100

    Securing the API

--- sst.config.ts
+++ sst.config.ts
@@ -6,14 +6,22 @@ export default $config({
       name: "sst-v3-notes",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
-      providers:{
-        aws:{profile: "sst-v3"}
-      }
+      providers: {
+        aws: { profile: "sst-v3" },
+      },
     };
   },
   async run() {
     await import("./infra/storage");
     await import("./infra/api");
 
+    const auth = await import("./infra/auth");
+
+    return {
+      UserPool: auth.userPool.id,
+      Region: aws.getRegionOutput().name,
+      IdentityPool: auth.identityPool.id,
+      UserPoolClient: auth.userPoolClient.id,
+    };
   },
 });
