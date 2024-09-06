a692c6c0b5b3ddbeb85621556df8b320b6ccf1b8
Paul Bacchus <paulbacchus@M35-117.local>
2024-09-06 15:26:26 +0100

    Building our React app

--- packages/frontend/src/config.ts
+++ packages/frontend/src/config.ts
@@ -1,4 +1,8 @@
 const config = {
+  STRIPE_KEY:
+    "pk_test_51OMWrRA4XEfwoLstmxABvi9pKWs8zHdBpSax6NRNNrevJo46e6k0AM5YmNqMao8FL3wjKt9CBbEneXNk6NhLWDD600kaxL88q1",
+  // Frontend config
+  MAX_ATTACHMENT_SIZE: 5000000,
   // Backend config
   s3: {
     REGION: import.meta.env.VITE_REGION,
